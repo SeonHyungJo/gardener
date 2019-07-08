cdf267a2a345ef0f3ed4fe1b8c7d1f29f648b712
SeonHyungJo <seonhyung.jo@gmail.com>
2019-07-08 22:13:06 +0900

    Auto-commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const shell = require("shelljs");
+const fs = require("fs");
+
+const utf8 = "utf8";
+const gardenName = "./garden.md";
+let DATE = new Date();
+
+// Git Auto Commit
+const autoCommit = () => {
+  if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
+    shell.echo("Error: Git commit failed");
+    shell.exit(1);
+  }
+
+  if (shell.exec('git push origin master').code !== 0) {
+    shell.echo("Error: Git commit failed");
+    shell.exit(1);
+  }
+};
+
+const giveWater = () => {
+  return new Promise((resolve, reject) => {
+    console.log("Write File ===>", `${DATE} 무럭 무럭 자라라`);
+    fs.readFile(gardenName, utf8, (err, data) => {
+      if (err) {
+        console.warn(err);
+        reject()
+      }
+
+      console.log("기존 내용 ===>", data);
+      const newText = data + `${DATE} 무럭 무럭 자라라 \n`;
+
+      fs.writeFile(gardenName, newText, utf8, () => {
+        if (err) {
+          console.warn(err);
+          reject()
+        }
+      });
+      resolve()
+    });
+  });
+};
+
+setInterval(()=>{
+  giveWater().then(autoCommit);
+}, 5000)
